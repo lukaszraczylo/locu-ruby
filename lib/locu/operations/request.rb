f350b21a29e0ee8dbde0faca32c110fb68877242
Lukasz Raczylo <lukasz.raczylo@digital.justice.gov.uk>
2017-10-01 15:22:03 +0100

    Use API host passed as a client parameter instead of hardcoded one

--- lib/locu/operations/request.rb
+++ lib/locu/operations/request.rb
@@ -8,9 +8,8 @@ module Locu
       end
 
       def execute(req_url = nil, req_data = nil)
-        base_url = 'https://api.locu.com/v2/'
         req_data["api_key"] = @client.configuration.app_user_key
-        obj = HTTParty.post("#{base_url}#{req_url.to_s}", :body => req_data.to_json).body
+        obj = HTTParty.post("#{@client.configuration.api_host}#{req_url.to_s}", :body => req_data.to_json).body
         JSON.parse(obj)
       end
     end
